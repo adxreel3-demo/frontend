2c9c587b3ee951043c0cc4696c01aae052674aa9
adxreel3-demo <adxreel3@gmail.com>
2026-01-09 16:14:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
 import AdGridPage from "./pages/AdGridPage";
 import AdDemoPage from "./pages/AdDemoPage";
+import "./styles/app.css";
 
 function App() {
   const [selectedAd, setSelectedAd] = useState(null);
- 
-  if (!selectedAd) {
-    return <AdGridPage onSelectAd={setSelectedAd} />;
-  }
 
-  return <AdDemoPage ad={selectedAd} />;
+  return (
+    <>
+      {/* ===== TOP HEADER ===== */}
+      <header className="top-header">
+        <div className="brand">
+          <img src="/logo.png" alt="ADXREEL Logo" />
+          <span>ADXREEL</span>
+        </div>
+      </header>
+
+      {/* ===== PAGE CONTENT ===== */}
+      {!selectedAd ? (
+        <AdGridPage onSelectAd={setSelectedAd} />
+      ) : (
+        <AdDemoPage ad={selectedAd} />
+      )}
+    </>
+  );
 }
 
 export default App;
