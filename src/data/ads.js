74bf9b5c9291224ccbb23a2b56b0cf7a909f8774
adxreel3-demo <adxreel3@gmail.com>
2026-01-09 16:42:47 +0530

    Update ads.js

--- src/data/ads.js
+++ src/data/ads.js
@@ -4,20 +4,26 @@ export const ADS = [
     companyName: "Bewakoof",
     productName: "Oversized T-Shirts",
     campaignId: 9301,
-    video: "/TshirtVideo.mp4"
+    video: "/TshirtVideo.mp4",
+    logo: "/bewakoof_logo.png",
+    banner: "/bewakoof_banner.jpg"
   },
   {
     adId: "titan_ad_1",
     companyName: "Titan Watches",
     productName: "Smart & Analog Watches",
     campaignId: 9201,
-    video: "/RolexWatch.mp4"
+    video: "/RolexWatch.mp4",
+    logo: "/titan_logo.png",
+    banner: "/titan_banner.jpg"
   },
   {
     adId: "cake_ad_1",
     companyName: "Cake Toppers India",
     productName: "Birthday Cake Toppers",
     campaignId: 9101,
-    video: "/Cake_Topper.mp4"
+    video: "/Cake_Topper.mp4",
+    logo: "/caketopper_logo.png",
+    banner: "/caketopper_banner.jpeg"
   }
 ];
